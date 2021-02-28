8780efbc41761a624e2a576425e91347c8d5ed65
wanglers <79512696+wanglers@users.noreply.github.com>
2021-02-28 14:05:28 +0100

    initial database

---
+++ database.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS surveys(  
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    name varchar(100) comment 'name of survey'
+) default charset utf8 comment '';
