71490616851f9179d25e97c880a043298f9d320b
wanglers <79512696+wanglers@users.noreply.github.com>
2021-03-11 20:46:00 +0100

    databas connect, setup php

---
+++ php/db/connect.php
@@ -0,0 +1,27 @@
+
+
+<?php
+function dbConnect()
+{
+    # code...
+
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+    return $conn;
+}
+?> 
+
+
+
+
+
