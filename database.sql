488801c41c022969833baa61d21441777e883780
wanglers <79512696+wanglers@users.noreply.github.com>
2021-04-10 17:13:48 +0200

    relation, värden i tabell

--- database.sql
+++ database.sql
@@ -5,4 +5,41 @@ CREATE TABLE IF NOT EXISTS surveys(
 SELECT
   *
 FROM
-  surveys
+  surveys;
+CREATE TABLE IF NOT EXISTS questions(
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    question varchar(100) comment 'the question',
+    answers int COMMENT 'the possible answers not nesesary?',
+    s_id int NOT NULL COMMENT 'Survey id',
+    answerslable VARCHAR (20) COMMENT 'answers display name not nesesary?'
+  ) default charset utf8 comment '';
+CREATE TABLE IF NOT EXISTS results(
+    id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+    q_id int NOT NULL COMMENT 'Question id',
+    result INT comment 'answer to question in survey'
+  ) default charset utf8 comment '';
+-- foreign keys
+ALTER TABLE
+  questions
+ADD
+  CONSTRAINT questions_surveys FOREIGN KEY (s_id) REFERENCES surveys (id);
+-- foreign keys
+ALTER TABLE
+  resluts
+ADD
+  CONSTRAINT resutls_questions FOREIGN KEY (q_id) REFERENCES questions (id);
+UPDATE
+  questions
+SET
+  s_id = 12,
+  question = 'Gillar du Sushi?'
+Where
+  id = 1;
+insert INTO
+  questions(question, s_id)
+VALUES
+  ('Gillar du sushi?', 12);
+insert INTO
+  results(q_id, result)
+VALUES
+  (1, 2)
